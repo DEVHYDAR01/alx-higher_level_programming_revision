b8e7a085bc31be4a63c43cbcffc1059847f19a39
DEVHYDAR01 <aliyuahmaad60@gmail.com>
2025-03-11 17:54:35 +0100

    This commit will push here are only 3 colors, 10 digits, and 7 notes; it's what we do with them that's important

--- 0x01-python-if_else_loops_functions/eight_uppercase.py
+++ 0x01-python-if_else_loops_functions/eight_uppercase.py
@@ -4,4 +4,5 @@ def uppercase(str):
         if ord(c) >= 97 and ord(c) <= 122:
             c = chr(ord(c) - 32)
             print("{}".format(c), end="")
-        print(c, end="")
+        else:
+            print(c, end="")
