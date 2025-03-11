c340cc893849330857415b78bb1b2a24f4d9bcdb
DEVHYDAR01 <aliyuahmaad60@gmail.com>
2025-03-11 11:54:47 +0100

    This commit will push 00...99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 
 for i in range(0, 100):
-    if i < 10:
-        print(f"0{i}", end=", ")
-    elif i == 99:
+    if i == 99:
         print(i)
     else:
-        print(i, end=", ")
+        print("{:02}".format(i), end=", ")
