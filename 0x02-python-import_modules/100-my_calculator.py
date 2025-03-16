ad53037e68c409c97ca6d3a82f29f10605578543
DEVHYDAR01 <aliyuahmaad60@gmail.com>
2025-03-17 00:00:21 +0100

    This commit will push Easy print

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -13,11 +13,7 @@ if not len(argv) - 1 == 3:
 if not argv[2] in operators:
     print("Unknown operator. Available operators: +, -, * and /")
     exit(1)
-# elif argv[2] == operators[0]:
-#     print("{} {} {} = {}".format(argv[1], argv[2], argv[3], add(int(argv[1]), int(argv[3]))))
-
 
 for i in range(len(operators)):
     if argv[2] == operators[i]:
-        # print(functions[i])
         print("{} {} {} = {}".format(argv[1], argv[2], argv[3], functions[i](int(argv[1]), int(argv[3]))))
