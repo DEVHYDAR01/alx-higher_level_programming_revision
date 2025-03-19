ded530484bdc387106a5dbfa29cb1979dbcabda0
DEVHYDAR01 <aliyuahmaad60@gmail.com>
2025-03-19 23:07:20 +0100

    This commit will push Delete at

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-my_list = [1, 2, 3, 4, 5]
-idx = 3
-
 def delete_at(my_list=[], idx=0):
     if idx < 0 or idx > len(my_list):
         return my_list
@@ -9,9 +6,3 @@ def delete_at(my_list=[], idx=0):
         if i == idx:
             del my_list[i]
     return my_list
-
-
-
-new_list = delete_at(my_list, idx)
-print(new_list)
-print(my_list)
