3dad9228fd4189e7975c08a3101a12ef73a574d4
DEVHYDAR01 <aliyuahmaad60@gmail.com>
2025-04-05 16:25:31 +0100

    This commit will push Print sorted dictionary

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -4,11 +4,3 @@ def number_keys(a_dictionary):
     for i in a_dictionary:
         count += 1
     return count
-
-
-
-
-
-a_dictionary = { 'language': "C", 'number': 13, 'track': "Low level" }
-nb_keys = number_keys(a_dictionary)
-print("Number of keys: {:d}".format(nb_keys))
