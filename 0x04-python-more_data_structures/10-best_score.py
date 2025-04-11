852e256aa49ffd2307c6a09008091af9fb6843b9
DEVHYDAR01 <aliyuahmaad60@gmail.com>
2025-04-11 20:24:53 +0100

    This commit will push  Multiply by using map

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -9,18 +9,3 @@ def best_score(a_dictionary):
             best_score = a_dictionary[key]
             bst_key = key
     return bst_key
-
-a_dictionary = {'John': 12, 'Bob': 14, 'Mike': 14, 'Molly': 16, 'Adam': 10}
-best_score(a_dictionary)
-best_key = best_score(a_dictionary)
-print("Best score: {}".format(best_key))
-
-best_key = best_score(None)
-print("Best score: {}".format(best_key))
-
-# list = [2, 5, 3, 10, 20, 6, 70, 8]
-# highest_val = 0
-# for i in list:
-#     if i > highest_val:
-#         highest_val = i
-# print(highest_val)
