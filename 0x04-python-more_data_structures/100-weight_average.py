1c22fcd3f4f06978a22846dbf0dc471c583a3237
DEVHYDAR01 <aliyuahmaad60@gmail.com>
2025-04-12 18:07:36 +0100

    This commit will push Squared by using map

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -9,12 +9,3 @@ def weight_average(my_list=[]):
         total_sum_of_divide_by += get_tuple_divide_by[i]
     weighted_average = total_sum_of_tuple / total_sum_of_divide_by 
     return weighted_average
-
-
-
-
-
-my_list = [(1, 2), (2, 1), (3, 10), (4, 2)]
-# = ((1 * 2) + (2 * 1) + (3 * 10) + (4 * 2)) / (2 + 1 + 10 + 2)
-result = weight_average(my_list)
-print("Average: {:0.2f}".format(result))
