8a1baf76185b1fe53a9975e263b117c78e2ab6be
DEVHYDAR01 <aliyuahmaad60@gmail.com>
2025-04-13 23:19:04 +0100

    This commit will push Safe list printing

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,32 +7,7 @@ def safe_print_list(my_list=[], x=0):
                 print(my_list[i], end="")
                 count += 1
             except IndexError:
-                print("\n[DEBUG] IndexError caught at index:", i)
                 break
     finally:
         print()
     return count
-
-
-
-my_list = [1, 2, 3, 4, 5]
-nb_print = safe_print_list(my_list, 2)
-# print(nb_print)
-print("nb_print: {:d}".format(nb_print))
-nb_print = safe_print_list(my_list, len(my_list))
-print("nb_print: {:d}".format(nb_print))
-nb_print = safe_print_list(my_list, len(my_list) + 2)
-print("nb_print: {:d}".format(nb_print))
-
-
-
-
-
-    # list_len = 0
-    # for i in my_list:
-    #     list_len += 1
-    # counter = 0
-    # new_list = [print("{}\n".format(my_list[i]), end="") for i in range(list_len) if not my_list[i] >= x+1]
-    # for nb in new_list:
-    #     counter += 1
-    # return counter
