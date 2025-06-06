1782f22881cbe167d1c9cffaa936b3fcd85339b5
DEVHYDAR01 <aliyuahmaad60@gmail.com>
2025-06-06 23:35:56 +0100

    This commit will push implementation of the function

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,25 +1,21 @@
 def matrix_divided(matrix, div):
-    row_size = 0
-    if not isinstance(matrix, (int, float)):
+    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
         raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-    for i in range(len(matrix)):
-        row_size = len(matrix[i])
-        if row_size != len(matrix[i]):
-            raise TypeError("Each row of the matrix must have the same size")
+    
+    if not all(isinstance(col, (int, float)) for row in matrix for col in row):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
+    
     if div == 0:
         raise ZeroDivisionError("division by zero")
-
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6]
-]
-# print(matrix_divided(matrix, 3))
-# for i in matrix:
-#     print(i)   
-# print(matrix)
-matrix_divided(matrix, 3)
+    
+    row_length = len(matrix[0])
+    if any(len(row) != row_length for row in matrix):
+        raise TypeError("Each row of the matrix must have the same size")
+    
+    return [[round(col / div, 2) for col in row] for row in matrix]
 
 
 
